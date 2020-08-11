e1de08876cf28989926f3b5df6367c78283b7b33
MAB <queenMab@MABpro2017.local>
2020-08-11 16:24:10 -0400

    clean up test-delivery console.logs, and add comments

--- src/twilio/functions/sms/test-delivery.js
+++ src/twilio/functions/sms/test-delivery.js
@@ -9,6 +9,10 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
   let code = event.Body;
   code = code.toUpperCase().trim();
 
+  // for this prototype, you text the code to a twilio number.
+  // in prod, the event will be a webhook from a front-end button
+
+  // below: looks up code in request Airtable, gets all important values
   base("Requests")
     .select({
       view: "Grid view", // from staging base
@@ -27,20 +31,20 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
         const volrecord = record.get("Intake volunteer");
         console.log(list);
 
+        // below: changes request status to "Delivery Assigned"
         base("Requests").update(
           record.id,
           {
             Status: "Delivery Assigned",
           },
-          function reAssign(reerror, assignrecord) {
-            if (err) {
-              console.error(err);
-              return;
+          function reAssign(reerror) {
+            if (reerror) {
+              console.error(reerror);
             }
-            console.log(assignrecord.get("Meta"));
           }
         );
 
+        // below, uses linked 'Intake volunteer' record id to get phone number and name from Volunteers table
         base("Volunteers").find(volrecord, function checkVolunteer(error, vrecord) { // eslint-disable-line
           if (error) {
             console.error(error);
@@ -51,10 +55,11 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
           const intakephone = vrecord.get("volunteer_phone");
           const intakename = vrecord.get("volunteer_name");
 
+          // sends SMS out to delivery volunteer
           const twilioClient = context.getTwilioClient();
           twilioClient.messages.create({
-            to: event.From,
-            from: event.To,
+            to: event.From, // need delivery volunteer phone number here
+            from: event.To, // will input twilio number here
             body: `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nTheir phone is ${phone}, you will need to get in touch with them about the full address. Their cross streets are ${street1} & ${street2}.\n\n${firstName}'s grocery list is: ${list}\n\nThe intake volunteer for this request is ${intakename}. Their phone # is ${intakephone}, and they can help if you have any questions - they'll reach out to you to follow up and make sure the delivery goes 👍`,
           }); // end twilio create sms
         });
