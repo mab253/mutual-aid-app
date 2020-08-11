c65361d4ffc86c06d098a5760f2a4176b62e61e4
MAB <queenMab@MABpro2017.local>
2020-08-11 16:13:41 -0400

    add intake volunteer info and reassigning, test-delivery twilio function

--- src/twilio/functions/sms/test-delivery.js
+++ src/twilio/functions/sms/test-delivery.js
@@ -5,13 +5,13 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
     apiKey: context.AIRTABLE_API_KEY // eslint-disable-line
   });
 
-  const base = Airtable.base("apppK7mrvMPcwtv6d");
+  const base = Airtable.base("apppPfEXed7SRcKmB"); // staging base
   let code = event.Body;
   code = code.toUpperCase().trim();
 
   base("Requests")
     .select({
-      view: "Only Delivery Needed",
+      view: "Grid view", // from staging base
       maxRecords: 1,
       filterByFormula: `({Code} = '${code}')`,
     })
@@ -24,13 +24,39 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
         const list = record.get("Intake General Notes");
         const street1 = record.get("Cross Street #1");
         const street2 = record.get("Cross Street #2");
+        const volrecord = record.get("Intake volunteer");
         console.log(list);
 
-        const twilioClient = context.getTwilioClient();
-        twilioClient.messages.create({
-          to: event.From,
-          from: event.To,
-          body: `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nTheir phone is ${phone}, you will need to get in touch with them about the full address. Their cross streets are ${street1} & ${street2}.\n\n${firstName}'s grocery list is: ${list}\n\n[INTAKE VOL], at [INTAKE PHONE], can help if you have any questions - they'll reach out to you to follow up and make sure the delivery goes 👍`,
+        base("Requests").update(
+          record.id,
+          {
+            Status: "Delivery Assigned",
+          },
+          function reAssign(reerror, assignrecord) {
+            if (err) {
+              console.error(err);
+              return;
+            }
+            console.log(assignrecord.get("Meta"));
+          }
+        );
+
+        base("Volunteers").find(volrecord, function checkVolunteer(error, vrecord) { // eslint-disable-line
+          if (error) {
+            console.error(error);
+            return;
+          }
+          console.log("Retrieved", vrecord.id);
+          console.log("Intake phone:", vrecord.get("volunteer_phone"));
+          const intakephone = vrecord.get("volunteer_phone");
+          const intakename = vrecord.get("volunteer_name");
+
+          const twilioClient = context.getTwilioClient();
+          twilioClient.messages.create({
+            to: event.From,
+            from: event.To,
+            body: `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nTheir phone is ${phone}, you will need to get in touch with them about the full address. Their cross streets are ${street1} & ${street2}.\n\n${firstName}'s grocery list is: ${list}\n\nThe intake volunteer for this request is ${intakename}. Their phone # is ${intakephone}, and they can help if you have any questions - they'll reach out to you to follow up and make sure the delivery goes 👍`,
+          }); // end twilio create sms
         });
       });
     });
