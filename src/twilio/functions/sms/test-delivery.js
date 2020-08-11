108c9401bd620929c9ef0ec6d405c46dbd495eb9
MAB <queenMab@MABpro2017.local>
2020-08-11 17:31:46 -0400

    fix twilio callback, sms/test-delivery function

--- src/twilio/functions/sms/test-delivery.js
+++ src/twilio/functions/sms/test-delivery.js
@@ -12,6 +12,8 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
   // for this prototype, you text the code to a twilio number.
   // in prod, the event will be a webhook from a front-end button
 
+  console.log("incoming message received");
+
   // below: looks up code in request Airtable, gets all important values
   base("Requests")
     .select({
@@ -29,7 +31,6 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
         const street1 = record.get("Cross Street #1");
         const street2 = record.get("Cross Street #2");
         const volrecord = record.get("Intake volunteer");
-        console.log(list);
 
         // below: changes request status to "Delivery Assigned"
         base("Requests").update(
@@ -45,26 +46,31 @@ exports.handler = function codeSms(context, event, callback) { // eslint-disable
         );
 
         // below, uses linked 'Intake volunteer' record id to get phone number and name from Volunteers table
-        base("Volunteers").find(volrecord, function checkVolunteer(error, vrecord) { // eslint-disable-line
-          if (error) {
-            console.error(error);
+        base("Volunteers").find(volrecord, function checkVolunteer(volerror, vrecord) { // eslint-disable-line
+          if (volerror) {
+            console.error(volerror);
             return;
           }
           console.log("Retrieved", vrecord.id);
-          console.log("Intake phone:", vrecord.get("volunteer_phone"));
           const intakephone = vrecord.get("volunteer_phone");
           const intakename = vrecord.get("volunteer_name");
 
           // sends SMS out to delivery volunteer
           const twilioClient = context.getTwilioClient();
-          twilioClient.messages.create({
-            to: event.From, // need delivery volunteer phone number here
-            from: event.To, // will input twilio number here
-            body: `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nTheir phone is ${phone}, you will need to get in touch with them about the full address. Their cross streets are ${street1} & ${street2}.\n\n${firstName}'s grocery list is: ${list}\n\nThe intake volunteer for this request is ${intakename}. Their phone # is ${intakephone}, and they can help if you have any questions - they'll reach out to you to follow up and make sure the delivery goes 👍`,
-          }); // end twilio create sms
-        });
-      });
-    });
-
-  callback();
+          twilioClient.messages.create(
+            {
+              to: event.From, // need delivery volunteer phone number here
+              from: event.To, // will input twilio number here
+              body: `Thanks for taking on this delivery for ${firstName}!\nCODE = ${code}.\n\nTheir phone is ${phone}, you will need to get in touch with them about the full address. Their cross streets are ${street1} & ${street2}.\n\n${firstName}'s grocery list is: ${list}\n\nThe intake volunteer for this request is ${intakename}. Their phone # is ${intakephone}, and they can help if you have any questions - they'll reach out to you to follow up and make sure the delivery goes 👍`,
+            },
+            function smsCallback(error, result) {
+              console.log("Created message using callback");
+              console.log(result.sid);
+              // callback needs to go here, or it ends the function before async sending sms
+              callback();
+            }
+          ); // end twilio create sms
+        }); // end find volrecord
+      }); // end getRecordInfo
+    }); // end seekRecords
 };
